6e91a73d894685bb8ab6885d03b0c1634b3fc305
dinhvanlanhit <39582595+dinhvanlanhit@users.noreply.github.com>
2021-08-22 19:31:59 +0700

    Update single.js

--- public/assets/web/single/single.js
+++ public/assets/web/single/single.js
@@ -10,6 +10,10 @@ function single(){
         $(document).ready(function(){
             hljs.configure({languages: ['javascript','typescript', 'ruby', 'python','php','xml','dockerfile','css','scss','csharp','json','php-template','sql','swift']});
             contentDarkMode();
+            $('.post-content').find('img').removeAttr('style');
+            $('.post-content').find('img').removeAttr('width');
+            $('.post-content').find('img').removeAttr('height');
+            $('.post-content').find('img').css('img-fluid img-thumbnail');
         });
 
     }
