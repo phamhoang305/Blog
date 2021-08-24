30f4da447fb5199098b6c11e2546b20e94dc15c9
dinhvanlanhit <39582595+dinhvanlanhit@users.noreply.github.com>
2021-08-24 11:59:41 +0700

    lành

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,3 +20,7 @@ mix.scripts([
 ],'public/assets/themes/app/script.js').minify('public/assets/themes/app/script.js').sourceMaps();
 mix.styles(['public/assets/main/dev.css'],"public/assets/main/main.css").minify('public/assets/main/main.css').sourceMaps();;
 mix.scripts(['public/assets/main/dev.js'],"public/assets/main/main.js").minify('public/assets/main/main.js').sourceMaps();;
+// mix.minTemplate = require('laravel-mix-template-minifier')
+// if (mix.inProduction()) {
+//   mix.minTemplate('storage/framework/views/*.php', 'storage/framework/views/')
+// }
