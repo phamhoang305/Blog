1be1c53020f094a2a09fc092621a8e9610bd892e
dinhvanlanhit <39582595+dinhvanlanhit@users.noreply.github.com>
2021-09-01 17:04:17 +0700

    Update script.blade.php

--- resources/views/web/includes/script.blade.php
+++ resources/views/web/includes/script.blade.php
@@ -9,18 +9,6 @@ var _cate_slug = "@isset($post->cate_slug) {{$post->cate_slug}} @endisset";
 @else
 <script src="{{asset('assets/main//main.min.js')}}?v={{uniqid()}}"></script>
 @endif
-<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-<script>
-  window.OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: "88bc549a-ca99-43e4-9ffc-6108da4a7c78",
-      safari_web_id: "web.onesignal.auto.5c6acdd7-2576-4d7e-9cb0-efba7bf8602e",
-      notifyButton: {
-        enable: true,
-      },
-    });
-  });
-</script>
+
 
 
